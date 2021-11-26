d87eaa47930d0bc6f36bcad35af468d4bf5c5d84
ChristoferSchwarz <95073342+ChristoferSchwarz@users.noreply.github.com>
2021-11-26 09:26:32 +0100

    Initiate website

---
+++ smooth-scroll.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$(document).on('click', 'a[href^="#"]', function (event){
+event.preventDefault();
+$('html, body').animate({
+scrollTop: $($.attr(this, 'href')).offset().top
+}, 750);
+});
